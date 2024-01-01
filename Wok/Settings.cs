e746c3eb95a3293f6dac92dd6cfd21cf8e5e7d16
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-01 22:26:04 +0200

    squash!

--- Wok/Settings.cs
+++ Wok/Settings.cs
@@ -7,13 +7,11 @@
 		private void SettingChangingEventHandler(
 			object sender,
 			System.Configuration.SettingChangingEventArgs e
-		)
-		{ }
+		) { }
 
 		private void SettingsSavingEventHandler(
 			object sender,
 			System.ComponentModel.CancelEventArgs e
-		)
-		{ }
+		) { }
 	}
 }
